5547406dbeaab240b868e1aeaca2b51752360e10
jkipker <jkipker@gmail.com>
2016-06-20 13:56:39 -0500

    contact & services pages added

---
+++ contact.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Southern Synergy - Engineering Solutions</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/unslider.css">
+		<link rel="stylesheet" href="css/unslider-dots.css">
+		<link rel="stylesheet" href="css/styles.css">
+
+	</head>
+	<body>
+
+		<!-- Content -->
+		<div class="container">
+			<?php include ("modules/nav.php"); ?>
+
+			<div class="slider-container">
+				<div class="slider-image"><img src="img/headers/contact-header.jpg"/></div>
+			</div>
+			<div class="home-sub-section home-sub-gray">
+				<div class="content-section contact-section">				
+					<h2>Contact Us</h2>
+					<div class="section-break"></div>
+					<div class="section-description">SSI's staff consists of qualified professionals in all engineering disciplines related to the engineering, design and construction of manufacturing and process related facilities. Our services range from upstream and mid-stream oil through commissioning of process systems including detailed engineering, 3D design, procurement, and construction support. </div>
+
+					<div class="form-container split50">
+						<form class="form">						  
+						  <input type="text" name="name" placeholder="Name*"><br>
+						  <input type="text" name="email" placeholder="Email*">
+						  <textarea name="message" placeholder="Message"></textarea>
+						</form>
+						<div class="button send-btn">
+							<span class="button-copy">SEND</span>
+						</div>
+					</div>
+					<div class="form-copy split50">
+						<h4>Get in touch with us</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel enim nec turpis
+							<br/>
+						</p>
+						<p><a href="mailto: info@southernsynergy.com">info@southernsynergy.com</a></p>
+						<p><a href="tel:+1-256-353-8405">(256) 353-8405</a></p>
+						<p>	
+							Southern Synergy, Incorporated<br/>
+							P.O. Box 1706<br/>
+							17 Vine Street NW<br/>
+							Decatur, AL 35601
+						</p>
+					</div>
+				</div>
+			</div>
+
+
+			<?php include ("modules/footer.php"); ?>
+		</body>
+	</html>
---
+++ services.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Southern Synergy - Engineering Solutions</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/unslider.css">
+		<link rel="stylesheet" href="css/unslider-dots.css">
+		<link rel="stylesheet" href="css/styles.css">
+
+	</head>
+	<body>
+
+		<!-- Content -->
+		<div class="container">
+			<?php include ("modules/nav.php"); ?>
+
+			<div class="slider-container">
+				<div class="slider-image"><img src="img/headers/service-header.jpg"/></div>			
+			</div>
+
+			<div class="home-sub-section">
+				<div class="content-section services-section">								
+					<img src="img/services/serv-tmp.jpg"/>
+				</div>
+			</div>
+			<?php include ("modules/footer.php"); ?>
+		</body>
+	</html>
