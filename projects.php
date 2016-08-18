6b83ca265ea1d453e354f5bcfcab6f12d05e1b7b
ChopXII <mdlamb@chopwerk.com>
2016-08-18 15:02:32 -0500

    Half content added to projects

--- projects.php
+++ projects.php
@@ -15,10 +15,9 @@
 
 		<!-- Content -->
 		<div class="container inner-page-template">
-			<?php include ("modules/nav.php"); ?>
-
-			<div class="slider-container">
-				<div class="slider-image"><img src="img/headers/about-header.jpg"/></div>	
+		<?php include ("modules/nav.php"); ?>
+		<div class="slider-container">
+		  <div class="slider-image"><img src="img/headers/about-header.jpg"/></div>	
 				<div class="header-shadow inner-head-shadow"></div>		
 			</div>
 
@@ -36,13 +35,14 @@
 			</div>
 
 
-
+<!--//PROJECT ONE//-->
 			<div class="home-sub-section home-sub-gray">
 				<div class="content-section services-section">
 
-					<h2>Project One - Mechanical</h2>					
-
-					<div class="split50 split70">
+					<h2 style="color:#1896d5;">Project One - Montana Refinery – Low Sulphur Fuels Expansion Project</h2>					
+<h3 style="margin-left:0px;">Description</h3>
+                        <p style="margin-left:0px; ">The $425,000,000 Refinery Expansion Project where SSI supported OSBL Expansion with mechanical, civil, and structural engineering design.  Engineering scope included general arrangement development, civil site design including earthen containment for tank farms, fire protection piping, process sewer, pump containment, hydraulic analysis, coalescing and filtration equipment size and selection, process flow diagrams, piping and instrumentation diagrams, equipment, pumps, piping, piping stress analysis, structural steel platforms, structural steel calculations, civil concrete foundations including tank ringwalls and drilled piers, field engineering support and project management. </p>
+					<div class="split50 split70" style="width:100%">
 						<div class="w3-content" style="max-width:1200px">
 						  <img class="mySlides" src="img/project-slides/slide-tmp1.jpg" style="width:100%">
 						  <img class="mySlides" src="img/project-slides/slide-tmp2.jpg" style="width:100%">
@@ -66,31 +66,46 @@
 						</div>
 					</div>
 
-					<div class="split50 split30">
-						<h3>Features</h3>
+					<div class="split50 split30" style="font-size:14px; display:block; width:100%;">
+						
+                        <h3>Features</h3>
 						<ul>
-							<li>Capital Project Development w/TICe</li>
-							<li>Equipment Specification Development</li>
-							<li>Pipe Stress Analysis (Caesar II)</li>
-							<li>ASME Pressure Vessel Design (PV Elite)</li>
-							<li>3D Plant Equipment Layout (CADWorx)</li>
-							<li>Piping Routing and Isometrics</li>
-							<li>Piping Hydraulic and Sizing Calculations</li>
-							<li>HVAC specification and load calculations</li>
-							<li>Root Cause Failure Analysis (RFCA)</li>
-						</ul>
+							<li>Scope of Work Development, Engineering Estimation, and Total Installed Cost Estimation</li>
+							<li>Civil and Structural Design scope included:<br/>
+                            <ul>
+                            	<li>General Site Arrangement Development</li>
+                            	<li>Tank Farm Layout : Containment Calculations and Earthen Berm Gradework Design</li>
+								<li>Tank Ringwall Foundation Design</li>
+								<li>Pump and Coalescer Containment Areas </li>
+								<li>Structural Analysis for Member Sizing and Foundation Design</li>
+								<li>Structural Pipe Rack and Personnel Platform Design</li>
+                        </ul>                          
+ 						</li>
+
+							<li>Development of Process Flow Diagrams and P&IDs for OSBL Expansion Configuration </li>
+							<li>Mechanical Design scope included:<br/>
+                        	<ul>
+								<li>Piping Design to include sizing and stress analysis and isometric fabrication drawing development</li>
+								<li>Pump and Filtration Equipment to include hydraulic system analysis</li>
+								<li>Fire Protection System piping and equipment design for Tank Farms</li>
+								<li>Utilized Navisworks 3D site modelling for preliminary and final design reviews with client</li>
+								<li>Base Map development for plant safety and environmental use to include excavation permitting</li>
+							<li>Piping and equipment design for railcar loading, unloading, and metering stations</li>
+
+							</ul>
 					</div>
-					
 				</div>
 			</div>
 
 
-
+<!--//PROJECT TWO//-->
 			<div class="home-sub-section">
 				<div class="content-section services-section">								
-					<h2>Project Two - Electrical</h2>					
+					<h2 style="color:#1896d5;">Project Two - Midstream Oil - Central Distribution Point</h2>					
+<h3 style="margin-left:0px;">Description</h3>
+                        <p style="margin-left:0px; ">The $65,000,000 Expansion Project that included legal for trade truck loading terminal, Four 10,000 BBL storage tanks, pump house, MCC and flare System.  Engineering scope included, piping and instrumentation diagrams, process vessels, equipment, pumps, piping, drilled pier tank foundation, instrumentation, controls, electrical power requirements, DCS Programming, project scheduling, construction management, commissioning and start-up. </p>				
 
-					<div class="split50 split70">
+					<div class="split50 split70" style="width:100%">
 						
 						<div class="w3-content" style="max-width:1200px">
 						  <img class="mySlides2" src="img/project-slides/slide-tmp1.jpg" style="width:100%">
@@ -115,26 +130,104 @@
 						</div>
 					</div>
 
-					<div class="split50 split30">
-						<h3>Features</h3>
+					<div class="split50 split30" style="font-size:14px; display:block; width:100%;">
+						
+                        <h3>Features</h3>
 						<ul>
-							<li>Capital Project Development w/TICe</li>
-							<li>Equipment Specification Development</li>
-							<li>Pipe Stress Analysis (Caesar II)</li>
-							<li>ASME Pressure Vessel Design (PV Elite)</li>
-							<li>3D Plant Equipment Layout (CADWorx)</li>
-							<li>Piping Routing and Isometrics</li>
-							<li>Piping Hydraulic and Sizing Calculations</li>
-							<li>HVAC specification and load calculations</li>
-							<li>Root Cause Failure Analysis (RFCA)</li>
+							<li>Scope of Work Development, Engineering Estimation, and Total Installed Cost Estimation</li>
+							<li>Civil and Structural Design scope included:<br/>
+                            <ul>
+                            	<li>General Site Arrangement Development</li>
+                            	<li>4 – 10,000 BBL Oil Storage Tanks with Earthen Berm </li>
+								<li>Drilled Pier Tank Foundation </li>
+								<li>4 – 600 GPM Pumps with Building and Foundation  </li>
+                            </ul>                          
+ 						</li>
+
+							<li>Development of Piping and instrumentation Diagrams</li>
+							<li>Design and Project Support scope included:<br/>
+                        	<ul>
+								<li>Piping Design to include sizing and stress analysis and isometric fabrication drawing development</li>
+								<li>Pump and Filtration Equipment to include hydraulic system analysis</li>
+								<li>Certified Legal for Trade Truck Loading Terminal</li>
+								<li>New Flare Addition for Truck Loading Terminal</li>
+								<li>Piping and equipment design for Tank Storage/Truck loading and metering stations</li>
+								<li>Piping Design for Pig Receiving Station on Main Crude Receiving Pipeline</li>
+                                <li>Develop complete 3D Model</li>
+								<li>Commissioning, DCS Programming, loop check and start up support at CDP</li>
+
 						</ul>
 					</div>
+                    
 				<br/><br/>
 			</div>
+<!--//PROJECT THREE//-->
+			<div class="home-sub-section home-sub-gray">
+				<div class="content-section services-section">
+
+					<h2 style="color:#1896d5;">Project Three - Upstream Oil – Well Pad Designs</h2>					
+<h3 style="margin-left:0px;">Description</h3>
+                        <p style="margin-left:0px; ">SSI has worked with in Upstream Oil for the past three years developing more than 18 well pad designs.  SSI has optimized their process while reducing their installation cost due to our development of a full 3D model, fabrication isometrics, construction drawings, bid package.  This has provided WPX the opportunity to competitively  bid the construction while standardizing their equipment/components. </p>
+					<div class="split50 split70" style="width:100%">
+						<div class="w3-content" style="max-width:1200px">
+						  <img class="mySlides" src="img/project-slides/slide-tmp1.jpg" style="width:100%">
+						  <img class="mySlides" src="img/project-slides/slide-tmp2.jpg" style="width:100%">
+						  <img class="mySlides" src="img/project-slides/slide-tmp3.jpg" style="width:100%">
+						   <img class="mySlides" src="img/project-slides/slide-tmp4.jpg" style="width:100%">
+
+						  <div class="w3-row-padding w3-section">
+						    <div class="w3-col s3">
+						      <img class="demo w3-border w3-hover-shadow" src="img/project-slides/slide-tmp1.jpg" style="width:100%" onclick="currentDiv(1)">
+						    </div>
+						    <div class="w3-col s3">
+						      <img class="demo w3-border w3-hover-shadow" src="img/project-slides/slide-tmp2.jpg" style="width:100%" onclick="currentDiv(2)">
+						    </div>
+						    <div class="w3-col s3">
+						      <img class="demo w3-border w3-hover-shadow" src="img/project-slides/slide-tmp3.jpg" style="width:100%" onclick="currentDiv(3)">
+						    </div>
+						     <div class="w3-col s3">
+						      <img class="demo w3-border w3-hover-shadow" src="img/project-slides/slide-tmp4.jpg" style="width:100%" onclick="currentDiv(4)">
+						    </div>
+						  </div>
+						</div>
+					</div>
+
+					<div class="split50 split30" style="font-size:14px; display:block; width:100%;">
+						
+                        <h3>Features</h3>
+						<ul>
+							<li>Scope of Work Development, Engineering Estimation, and Total Installed Cost Estimation</li>
+							<li>Civil and Structural Design scope included:<br/>
+                            <ul>
+                            	<li>General Site Arrangement Development</li>
+                            	<li>Tank Farm Layout : Containment Calculations and Earthen Berm Gradework Design</li>
+								<li>Tank Ringwall Foundation Design</li>
+								<li>Pump and Coalescer Containment Areas </li>
+								<li>Structural Analysis for Member Sizing and Foundation Design</li>
+								<li>Structural Pipe Rack and Personnel Platform Design</li>
+                        </ul>                          
+ 						</li>
+
+							<li>Development of Process Flow Diagrams and P&IDs for OSBL Expansion Configuration </li>
+							<li>Mechanical Design scope included:<br/>
+                        	<ul>
+								<li>Piping Design to include sizing and stress analysis and isometric fabrication drawing development</li>
+								<li>Pump and Filtration Equipment to include hydraulic system analysis</li>
+								<li>Fire Protection System piping and equipment design for Tank Farms</li>
+								<li>Utilized Navisworks 3D site modelling for preliminary and final design reviews with client</li>
+								<li>Base Map development for plant safety and environmental use to include excavation permitting</li>
+							<li>Piping and equipment design for railcar loading, unloading, and metering stations</li>
+
+							</ul>
+					</div>
+				</div>
+			</div>
+            
+            
 			<?php include ("modules/footer.php"); ?>
 
 			<script>
-				//PROEJCT GALELRY SLDIESHOW FUNCTIONS
+				//PROJECT GALLERY SLDIESHOW FUNCTIONS
 			    var slideIndex = 1;
 				showDivs(slideIndex);
 				showDivs2(slideIndex);
